5f21f793c38b33a2eddc268997c14cf689d38452
thimnaalam <alamthimna@gmail.com>
2024-02-09 16:15:17 +0200

    functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Welcome to Express Js");
+
+
+// ES
+import  express from 'express' 
+// const express = require('express')   ----command js
+// create express app
+
+const app = express()
+const port = +process.env.PORT || 4000
+// Router -- where you going , app get from which is the /
+app.get('/', (req, res)=>{
+res.json({
+    status: res.statusCode,
+    message: 'Home'
+})
+})
+app.get('/about', (req, res)=>{
+    res.json({
+        status: res.statusCode,
+        message: 'about'
+    })
+    })
+    app.get('*', (req, res)=>{
+        res.json({
+            status: res.statusCode,
+            message: 'error 404'
+        })
+        })
+
+app.listen(port)
