af8c8d78e7b0d353da345b461c292b496a8fd1c2
thimnaalam <alamthimna@gmail.com>
2024-02-13 08:37:42 +0200

    update

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 
 
 // ES
-import  express from 'express' 
+import  express from 'express'
+import path from 'path' 
 // const express = require('express')   ----command js
 // create express app
-
 const app = express()
+// router middleware to be more specific
+const router = express.Router()
+app.use(
+    router,
+    express.static('./static')
+    )   
 const port = +process.env.PORT || 4000
 // Router -- where you going , app get from which is the /
-app.get('/', (req, res)=>{
-res.json({
-    status: res.statusCode,
-    message: 'Home'
+router.get('^/$|/express',display, (req, res)=>{
+res.status(200).sendFile(path.resolve('./static/html/index.html'))
 })
+router.get('/about',(req, res)=>{
+    res.json({
+
+    })
 })
+//Middleware
+function display(req,res,next){
+    console.log("Hello Papi");
+    next()
+}
+
+
+
 app.get('/about', (req, res)=>{
     res.json({
         status: res.statusCode,
